2bbea47e032ec3d7657fbadfebd1c70c88944df5
Chinmay Patil <chinmaytlc@gmail.com>
2021-07-27 10:42:17 -0600

    More testing

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const http = require('http');
 
 try {
   // `who-to-greet` input defined in action metadata file
@@ -8,8 +9,27 @@ try {
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  //const payload = JSON.stringify(github.context.payload, undefined, 2)
+  // console.log(`The event payload: ${payload}`);
+  console.log("")
+  console.log("")
+  const apiToken = core.getInput('EsoUIToken');
+
+
+  let options = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'x-api-token': apiToken
+    }
+  }
+
+  req = http.request('https://api.esoui.com/addons/list.json', options, (res) => {
+    console.log(JSON.stringify(res))
+  })
+
+
+
 } catch (error) {
   core.setFailed(error.message);
 }
