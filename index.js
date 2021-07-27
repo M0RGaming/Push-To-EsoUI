92a68adbab5faf74bb6a48b6a9d3640ebf29bd85
Chinmay Patil <chinmaytlc@gmail.com>
2021-07-27 10:46:32 -0600

    more things

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const http = require('http');
+const http = require('https');
 
 try {
   // `who-to-greet` input defined in action metadata file
